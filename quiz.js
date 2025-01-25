7c413aacd34fa28a06633cdc759e9b3f5e7b1391
Inene Israel Oluwaniyi <Israelinene91@gmail.com>
2025-01-25 23:17:13 +0100

    task 1 solution

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer() {
+  const correctAnswer = 4;
+  userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+  const feedback = document.getElementById("feedback");
+  if (correctAnswer == userAnswer) {
+    feedback.innerHTML = "Correct! Well done.";
+  } else {
+    feedback.innerHTML = "That's incorrect. Try again!";
+  }
+  console.log(userAnswer);
+}
+
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
