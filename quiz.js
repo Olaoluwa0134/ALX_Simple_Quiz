d60bcb0d793b17214b590ef0db448c2dc19a30e4
Inene Israel Oluwaniyi <Israelinene91@gmail.com>
2025-01-25 23:37:24 +0100

    strict condition

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer() {
   const correctAnswer = 4;
   userAnswer = document.querySelector('input[name="quiz"]:checked').value;
   const feedback = document.getElementById("feedback");
-  if (correctAnswer == userAnswer) {
+  if (correctAnswer === userAnswer) {
     feedback.innerHTML = "Correct! Well done.";
   } else {
     feedback.innerHTML = "That's incorrect. Try again!";
